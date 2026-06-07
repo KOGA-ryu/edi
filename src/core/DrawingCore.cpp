84d2f03437e95a53010c4693c7c4915f2624973c
Ace <dethislikethewind@gmail.com>
2026-06-07 06:52:00 -0600

    Use typed shape dispatch for SVG export

--- src/core/DrawingCore.cpp
+++ src/core/DrawingCore.cpp
@@ -1610,6 +1610,82 @@ QString svgPointAttributes(const QJsonObject &object, const QString &fallbackStr
     attributes += svgMetadataAttributes(object);
     return attributes;
 }
+
+void appendSerializedObjectToSvg(QString &svg, const QJsonObject &object) {
+    const QString kind = object.value("kind").toString();
+    const ShapeKind shapeKind = objectShapeKind(object);
+    switch (shapeKind) {
+    case ShapeKind::Point: {
+        const QJsonArray point = object.value("point_px").toArray();
+        if (kind == QStringLiteral("tone_probe")) {
+            svg += QStringLiteral("    <circle id=\"%1\" cx=\"%2\" cy=\"%3\" r=\"7\"%4/>\n")
+                .arg(svgEscaped(object.value("id").toString()), svgNumber(point.at(0).toDouble()), svgNumber(point.at(1).toDouble()), svgPointAttributes(object, QStringLiteral("#70d6ff")));
+        } else {
+            svg += QStringLiteral("    <circle id=\"%1\" cx=\"%2\" cy=\"%3\" r=\"5\"%4/>\n")
+                .arg(svgEscaped(object.value("id").toString()), svgNumber(point.at(0).toDouble()), svgNumber(point.at(1).toDouble()), svgPointAttributes(object));
+        }
+        break;
+    }
+    case ShapeKind::Line: {
+        const QJsonArray from = object.value("from_px").toArray();
+        const QJsonArray to = object.value("to_px").toArray();
+        if (kind == QStringLiteral("glyph_baseline")) {
+            svg += QStringLiteral("    <line id=\"%1\" x1=\"%2\" y1=\"%3\" x2=\"%4\" y2=\"%5\"%6/>\n")
+                .arg(svgEscaped(object.value("id").toString()), svgNumber(from.at(0).toDouble()), svgNumber(from.at(1).toDouble()), svgNumber(to.at(0).toDouble()), svgNumber(to.at(1).toDouble()), svgCommonAttributes(object, false, QStringLiteral("#70d6ff")));
+        } else {
+            svg += QStringLiteral("    <line id=\"%1\" x1=\"%2\" y1=\"%3\" x2=\"%4\" y2=\"%5\"%6/>\n")
+                .arg(svgEscaped(object.value("id").toString()), svgNumber(from.at(0).toDouble()), svgNumber(from.at(1).toDouble()), svgNumber(to.at(0).toDouble()), svgNumber(to.at(1).toDouble()), svgCommonAttributes(object, false));
+        }
+        break;
+    }
+    case ShapeKind::Circle: {
+        const QJsonArray center = object.value("center_px").toArray();
+        if (kind == QStringLiteral("arc")) {
+            const double radius = object.value("radius_px").toDouble();
+            const double start = degreesToRadians(object.value("start_angle_deg").toDouble());
+            const double end = degreesToRadians(object.value("end_angle_deg").toDouble());
+            const double x1 = center.at(0).toDouble() + std::cos(start) * radius;
+            const double y1 = center.at(1).toDouble() + std::sin(start) * radius;
+            const double x2 = center.at(0).toDouble() + std::cos(end) * radius;
+            const double y2 = center.at(1).toDouble() + std::sin(end) * radius;
+            const int largeArc = std::abs(object.value("end_angle_deg").toDouble() - object.value("start_angle_deg").toDouble()) > 180.0 ? 1 : 0;
+            svg += QStringLiteral("    <path id=\"%1\" d=\"M %2 %3 A %4 %4 0 %5 1 %6 %7\"%8/>\n")
+                .arg(svgEscaped(object.value("id").toString()), svgNumber(x1), svgNumber(y1), svgNumber(radius), QString::number(largeArc), svgNumber(x2), svgNumber(y2), svgCommonAttributes(object, false));
+        } else {
+            svg += QStringLiteral("    <circle id=\"%1\" cx=\"%2\" cy=\"%3\" r=\"%4\"%5/>\n")
+                .arg(svgEscaped(object.value("id").toString()), svgNumber(center.at(0).toDouble()), svgNumber(center.at(1).toDouble()), svgNumber(object.value("radius_px").toDouble()), svgCommonAttributes(object, true));
+        }
+        break;
+    }
+    case ShapeKind::Rectangle: {
+        const QJsonArray rect = object.value("rect_px").toArray();
+        if (kind == QStringLiteral("rectangle")) {
+            svg += QStringLiteral("    <rect id=\"%1\" x=\"%2\" y=\"%3\" width=\"%4\" height=\"%5\"%6/>\n")
+                .arg(svgEscaped(object.value("id").toString()), svgNumber(rect.at(0).toDouble()), svgNumber(rect.at(1).toDouble()), svgNumber(rect.at(2).toDouble()), svgNumber(rect.at(3).toDouble()), svgCommonAttributes(object, true));
+        } else if (kind == QStringLiteral("image_reference_frame")) {
+            svg += QStringLiteral("    <rect id=\"%1\" x=\"%2\" y=\"%3\" width=\"%4\" height=\"%5\"%6/>\n")
+                .arg(svgEscaped(object.value("id").toString()), svgNumber(rect.at(0).toDouble()), svgNumber(rect.at(1).toDouble()), svgNumber(rect.at(2).toDouble()), svgNumber(rect.at(3).toDouble()), svgCommonAttributes(object, true, QStringLiteral("#70d6ff")));
+        } else if (kind == QStringLiteral("ascii_crop_frame")) {
+            svg += QStringLiteral("    <rect id=\"%1\" x=\"%2\" y=\"%3\" width=\"%4\" height=\"%5\"%6/>\n")
+                .arg(svgEscaped(object.value("id").toString()), svgNumber(rect.at(0).toDouble()), svgNumber(rect.at(1).toDouble()), svgNumber(rect.at(2).toDouble()), svgNumber(rect.at(3).toDouble()), svgCommonAttributes(object, true, QStringLiteral("#ffcc66")));
+        } else {
+            svg += QStringLiteral("    <rect id=\"%1\" x=\"%2\" y=\"%3\" width=\"%4\" height=\"%5\"%6/>\n")
+                .arg(svgEscaped(object.value("id").toString()), svgNumber(rect.at(0).toDouble()), svgNumber(rect.at(1).toDouble()), svgNumber(rect.at(2).toDouble()), svgNumber(rect.at(3).toDouble()), svgCommonAttributes(object, true, QStringLiteral("#c084fc")));
+        }
+        break;
+    }
+    case ShapeKind::Polyline:
+        svg += QStringLiteral("    <polyline id=\"%1\" points=\"%2\"%3/>\n")
+            .arg(svgEscaped(object.value("id").toString()), svgEscaped(pointsToSvg(object.value("points_px").toArray())), svgCommonAttributes(object, false));
+        break;
+    case ShapeKind::Polygon:
+        svg += QStringLiteral("    <polygon id=\"%1\" points=\"%2\"%3/>\n")
+            .arg(svgEscaped(object.value("id").toString()), svgEscaped(pointsToSvg(object.value("points_px").toArray())), svgCommonAttributes(object, true));
+        break;
+    case ShapeKind::Unknown:
+        break;
+    }
+}
 } // namespace
 
 DrawingCoreResult DrawingCore::runScript(const QJsonObject &script) {
@@ -1762,89 +1838,8 @@ QString DrawingCore::modelToSvg(const QJsonObject &model) {
     svg += QStringLiteral("<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 %1 %1\" width=\"%1\" height=\"%1\">\n").arg(canvas);
     svg += QStringLiteral("  <g id=\"script_geometry\">\n");
     const QJsonArray objects = model.value("generated_objects").toArray();
-    struct ObjectRenderer {
-        const char *kind;
-        std::function<void(const QJsonObject &)> render;
-    };
-    const std::vector<ObjectRenderer> renderers = {
-        {"point", [&](const QJsonObject &object) {
-             const QJsonArray point = object.value("point_px").toArray();
-             svg += QStringLiteral("    <circle id=\"%1\" cx=\"%2\" cy=\"%3\" r=\"5\"%4/>\n")
-                 .arg(svgEscaped(object.value("id").toString()), svgNumber(point.at(0).toDouble()), svgNumber(point.at(1).toDouble()), svgPointAttributes(object));
-         }},
-        {"tone_probe", [&](const QJsonObject &object) {
-             const QJsonArray point = object.value("point_px").toArray();
-             svg += QStringLiteral("    <circle id=\"%1\" cx=\"%2\" cy=\"%3\" r=\"7\"%4/>\n")
-                 .arg(svgEscaped(object.value("id").toString()), svgNumber(point.at(0).toDouble()), svgNumber(point.at(1).toDouble()), svgPointAttributes(object, QStringLiteral("#70d6ff")));
-         }},
-        {"line", [&](const QJsonObject &object) {
-             const QJsonArray from = object.value("from_px").toArray();
-             const QJsonArray to = object.value("to_px").toArray();
-             svg += QStringLiteral("    <line id=\"%1\" x1=\"%2\" y1=\"%3\" x2=\"%4\" y2=\"%5\"%6/>\n")
-                 .arg(svgEscaped(object.value("id").toString()), svgNumber(from.at(0).toDouble()), svgNumber(from.at(1).toDouble()), svgNumber(to.at(0).toDouble()), svgNumber(to.at(1).toDouble()), svgCommonAttributes(object, false));
-         }},
-        {"glyph_baseline", [&](const QJsonObject &object) {
-             const QJsonArray from = object.value("from_px").toArray();
-             const QJsonArray to = object.value("to_px").toArray();
-             svg += QStringLiteral("    <line id=\"%1\" x1=\"%2\" y1=\"%3\" x2=\"%4\" y2=\"%5\"%6/>\n")
-                 .arg(svgEscaped(object.value("id").toString()), svgNumber(from.at(0).toDouble()), svgNumber(from.at(1).toDouble()), svgNumber(to.at(0).toDouble()), svgNumber(to.at(1).toDouble()), svgCommonAttributes(object, false, QStringLiteral("#70d6ff")));
-         }},
-        {"polyline", [&](const QJsonObject &object) {
-             svg += QStringLiteral("    <polyline id=\"%1\" points=\"%2\"%3/>\n")
-                 .arg(svgEscaped(object.value("id").toString()), svgEscaped(pointsToSvg(object.value("points_px").toArray())), svgCommonAttributes(object, false));
-         }},
-        {"circle", [&](const QJsonObject &object) {
-             const QJsonArray center = object.value("center_px").toArray();
-             svg += QStringLiteral("    <circle id=\"%1\" cx=\"%2\" cy=\"%3\" r=\"%4\"%5/>\n")
-                 .arg(svgEscaped(object.value("id").toString()), svgNumber(center.at(0).toDouble()), svgNumber(center.at(1).toDouble()), svgNumber(object.value("radius_px").toDouble()), svgCommonAttributes(object, true));
-         }},
-        {"arc", [&](const QJsonObject &object) {
-             const QJsonArray center = object.value("center_px").toArray();
-             const double radius = object.value("radius_px").toDouble();
-             const double start = degreesToRadians(object.value("start_angle_deg").toDouble());
-             const double end = degreesToRadians(object.value("end_angle_deg").toDouble());
-             const double x1 = center.at(0).toDouble() + std::cos(start) * radius;
-             const double y1 = center.at(1).toDouble() + std::sin(start) * radius;
-             const double x2 = center.at(0).toDouble() + std::cos(end) * radius;
-             const double y2 = center.at(1).toDouble() + std::sin(end) * radius;
-             const int largeArc = std::abs(object.value("end_angle_deg").toDouble() - object.value("start_angle_deg").toDouble()) > 180.0 ? 1 : 0;
-             svg += QStringLiteral("    <path id=\"%1\" d=\"M %2 %3 A %4 %4 0 %5 1 %6 %7\"%8/>\n")
-                 .arg(svgEscaped(object.value("id").toString()), svgNumber(x1), svgNumber(y1), svgNumber(radius), QString::number(largeArc), svgNumber(x2), svgNumber(y2), svgCommonAttributes(object, false));
-         }},
-        {"rectangle", [&](const QJsonObject &object) {
-             const QJsonArray rect = object.value("rect_px").toArray();
-             svg += QStringLiteral("    <rect id=\"%1\" x=\"%2\" y=\"%3\" width=\"%4\" height=\"%5\"%6/>\n")
-                 .arg(svgEscaped(object.value("id").toString()), svgNumber(rect.at(0).toDouble()), svgNumber(rect.at(1).toDouble()), svgNumber(rect.at(2).toDouble()), svgNumber(rect.at(3).toDouble()), svgCommonAttributes(object, true));
-         }},
-        {"image_reference_frame", [&](const QJsonObject &object) {
-             const QJsonArray rect = object.value("rect_px").toArray();
-             svg += QStringLiteral("    <rect id=\"%1\" x=\"%2\" y=\"%3\" width=\"%4\" height=\"%5\"%6/>\n")
-                 .arg(svgEscaped(object.value("id").toString()), svgNumber(rect.at(0).toDouble()), svgNumber(rect.at(1).toDouble()), svgNumber(rect.at(2).toDouble()), svgNumber(rect.at(3).toDouble()), svgCommonAttributes(object, true, QStringLiteral("#70d6ff")));
-         }},
-        {"ascii_crop_frame", [&](const QJsonObject &object) {
-             const QJsonArray rect = object.value("rect_px").toArray();
-             svg += QStringLiteral("    <rect id=\"%1\" x=\"%2\" y=\"%3\" width=\"%4\" height=\"%5\"%6/>\n")
-                 .arg(svgEscaped(object.value("id").toString()), svgNumber(rect.at(0).toDouble()), svgNumber(rect.at(1).toDouble()), svgNumber(rect.at(2).toDouble()), svgNumber(rect.at(3).toDouble()), svgCommonAttributes(object, true, QStringLiteral("#ffcc66")));
-         }},
-        {"ascii_cell_region", [&](const QJsonObject &object) {
-             const QJsonArray rect = object.value("rect_px").toArray();
-             svg += QStringLiteral("    <rect id=\"%1\" x=\"%2\" y=\"%3\" width=\"%4\" height=\"%5\"%6/>\n")
-                 .arg(svgEscaped(object.value("id").toString()), svgNumber(rect.at(0).toDouble()), svgNumber(rect.at(1).toDouble()), svgNumber(rect.at(2).toDouble()), svgNumber(rect.at(3).toDouble()), svgCommonAttributes(object, true, QStringLiteral("#c084fc")));
-         }},
-        {"polygon", [&](const QJsonObject &object) {
-             svg += QStringLiteral("    <polygon id=\"%1\" points=\"%2\"%3/>\n")
-                 .arg(svgEscaped(object.value("id").toString()), svgEscaped(pointsToSvg(object.value("points_px").toArray())), svgCommonAttributes(object, true));
-         }},
-    };
     for (const QJsonValue value : objects) {
-        const QJsonObject object = value.toObject();
-        const QString kind = object.value("kind").toString();
-        const auto renderer = std::find_if(renderers.begin(), renderers.end(), [&](const ObjectRenderer &entry) {
-            return kind == QString::fromLatin1(entry.kind);
-        });
-        if (renderer != renderers.end()) {
-            renderer->render(object);
-        }
+        appendSerializedObjectToSvg(svg, value.toObject());
     }
     svg += QStringLiteral("  </g>\n</svg>\n");
     return svg;
